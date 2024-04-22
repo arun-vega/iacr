de2c3f8ede4770f236445f921ecd1426d639fa8d
arun-sheri <142786118+arun-sheri@users.noreply.github.com>
2024-04-22 18:39:18 +0530

    implemented reason enroll and aboutus section

--- assets/javascript/testimonial.js
+++ assets/javascript/testimonial.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1200,
+    autoplaySpeed: 3000,
     arrows: false,
     dots: false,
     pauseOnHover: true,
